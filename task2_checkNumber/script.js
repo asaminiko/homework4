d044840207a173d58c1ea52a4e795d1d70002284
asaminiko <chebakova29@gmail.com>
2025-01-17 22:46:12 +0200

    remade task 2 and 3

--- task2_checkNumber/script.js
+++ task2_checkNumber/script.js
@@ -1,31 +1,38 @@
 function getInput() {
   let input = document.getElementById('input1').value
-  if (100 <= input && input <= 999) {
-    //перевірка на тризначність
-    console.log(`Число ${input} тризначне`)
-  } else {
-    console.log(`Помилка, ${input} не тризначне число`)
-    return false //функція зупинена
+  console.log(input)
+  function check(input) {
+    if (
+      input === undefined ||
+      input === null ||
+      input.length != 3 ||
+      (typeof input === 'string' && input.trim() === '')
+    ) {
+      //перевірка на undefined, null, пусту строку
+      return false
+    } else {
+      return input
+    }
   }
-  let i = 0
-  input.charAt(0) == input.charAt(1)
-    ? ++i
-    : console.log('цифри в 1 умові не однакові')
-  input.charAt(1) == input.charAt(2)
-    ? ++i
-    : console.log('цифри в 2 умові не однакові')
-  input.charAt(0) == input.charAt(2)
-    ? ++i
-    : console.log('цифри в 3 умові не однакові')
-  switch (i) {
-    case 3:
-      console.log('Так, всі цифри однакові')
-      break
-    case 1:
-      console.log('Є однакові цифри')
-      break
-    case 0:
-      console.log('Немає однакових цифр')
-      break
+  let inp = check(input)
+  if (inp === false) {
+    console.log('помилка')
+  } else {
+    let arr = inp.split('').map(Number) //додаю у масив
+    let i = 0
+    arr[0] == arr[1] ? ++i : false //порівнюю індекси
+    arr[1] == arr[2] ? ++i : false
+    arr[0] == arr[2] ? ++i : false
+    switch (i) {
+      case 3:
+        console.log('Так, всі цифри однакові')
+        break
+      case 1:
+        console.log('Є однакові цифри')
+        break
+      case 0:
+        console.log('Немає однакових цифр')
+        break
+    }
   }
 }
--- task3_userPortrait/script.js
+++ task3_userPortrait/script.js
@@ -5,54 +5,63 @@ function getInfo() {
   return { year, city, sport }
 }
 let info = getInfo()
-let array = []
+let array = [] // щоб дізнатись, що саме не ввів користувач
 function check(x) {
   if (x === undefined || x === null || x.trim() === '') {
+    //перевірка на undefined, null, пусту строку
     return false
   } else if (typeof x === 'string' && /^[a-zA-Zа-яА-ЯёЁіІїЇєЄґҐ\s]+$/.test(x)) {
+    //перевірка на символи
     return x
   } else {
     return false
   }
 }
 
-let y = info.year ? info.year : 0
+function age(y) {
+  if (y === undefined || y === null || isNaN(y) || y > 2024 || y < 1900) {
+    //перевірка на undefined, null, isNaN(), число більше 2024 або менше 1900
+    return false
+  } else if (typeof y === 'string' && y.trim() === '') {
+    //перевірка на пусту строку
+    return false
+  } else {
+    return 2025 - y
+    //рахую вік користувача
+  }
+}
+let y = age(info.year)
 let c = check(info.city)
 let s = check(info.sport)
 if (c === false) {
-  array.push('місто')
+  array.push('місто') // додаю у масив те, що не ввів користувач
   c = '...'
 }
 if (s === false) {
   array.push('спорт')
   s = '...'
 }
-function age() {
-  if (y === 0 || y > 2024) {
-    array.push('вік')
-    return 0
-  } else {
-    return 2025 - y
-  }
+if (y === false) {
+  array.push('вік')
+  y = '...'
 }
-
-c = c.charAt(0).toUpperCase() + c.slice(1)
-s = s.charAt(0).toLowerCase() + s.slice(1)
+c = c.trim().charAt(0).toUpperCase() + c.trim().slice(1).toLowerCase() // місто буде починатися з великої букви, а інші букви будуть з маленької. trim() прибирає пробіли
+s = s.trim().toLowerCase() //спорт буде складатися з маленьких букв. trim() прибирає пробіли
 console.log(`y ${y}`)
 console.log(`c ${c}`)
 console.log(`s ${s}`)
 switch (c) {
   case 'Київ':
-    alert(`Ти живеш у столиці України, твій вік ${age()}`)
+    alert(`Ти живеш у столиці України, твій вік ${y}`)
     break
   case 'Вашингтон':
-    alert(`Ти живеш у столиці США, твій вік ${age()}`)
+    alert(`Ти живеш у столиці США, твій вік ${y}`)
     break
   case 'Лондон':
-    alert(`Ти живеш у столиці Англії, твій вік ${age()}`)
+    alert(`Ти живеш у столиці Англії, твій вік ${y}`)
     break
   default:
-    alert(`Ти живеш у місті ${c}, твій вік ${age()}`)
+    alert(`Ти живеш у місті ${c}, твій вік ${y}`)
     break
 }
 switch (s) {
@@ -65,7 +74,12 @@ switch (s) {
   case 'фехтування':
     alert('Круто! Хочеш бути як Харлан Ольга?')
     break
+  case 'немає':
+    alert("Спорт корисний для твого здоров'я, пошукай що подобається саме тобі")
+    break
+  default: //додала default щоб виводився спорт у будь-якому випадку
+    alert(`Вид спорту: ${s}`)
 }
 if (array.length > 0) {
-  alert(`Шкода, що Ви не захотіли ввести ${array.join(', ')}`)
+  alert(`Шкода, що Ви не захотіли ввести ${array.join(', ')}`) //виводить якщо користувач щось не ввів
 }
