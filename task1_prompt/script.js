760e8c3e1fbc85fc33ae69006559342f9c5792ad
asaminiko <chebakova29@gmail.com>
2025-01-17 23:10:03 +0200

    remade task 1

--- task1_prompt/script.js
+++ task1_prompt/script.js
@@ -1,12 +1,24 @@
-const userName = prompt("ім'я користувач")
-const clearUserName = userName.trim() //ім'я без пропуску
+const userName = prompt("ім'я користувача")
+let realName = check(userName)
+const clearUserName = realName.trim() //ім'я без пропуску
 const firstletter = clearUserName.charAt(0) //перша літера
 const capitalLetter = firstletter.toUpperCase() //велика перша літера
+function check(x) {
+  if (x === undefined || x === null || x.trim() === '') {
+    //перевірка на undefined, null, пусту строку
+    console.log('помилка')
+    return '...'
+  } else if (typeof x === 'string' && /^[a-zA-Zа-яА-ЯёЁіІїЇєЄґҐ\s]+$/.test(x)) {
+    //перевірка на символи
+    return x
+  } else {
+    console.log('помилка')
+    return '...'
+  }
+}
 
-if (firstletter == capitalLetter) {
+if (firstletter == capitalLetter && clearUserName != '...') {
   alert(`Hello, ${clearUserName}! How are you?`)
 } else {
-  alert(
-    `${clearUserName} isn't a name, because a name must start with a capital letter`
-  )
+  alert(`${userName} isn't a name`)
 }
